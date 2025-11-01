f81cac42fb9e5dd21bcfd5f33e762221834f7e21
Veterok454 <releonora60@gmail.com>
2025-11-01 14:13:31 +0200

    project completed

--- frontend/src/components/NewsLetterBox.jsx
+++ frontend/src/components/NewsLetterBox.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NewsLetterBox = () => {
   return (
-    <div className='text-center'>
+    <div className='text-center mt-16 mb-16'>
       <div className='w-full flex justify-center mb-3'>
         <div className='flex flex-col sm:flex-row justify-around gap-2 sm:gap-4 text-center text-xs sm:text-sm md:text-base text-gray-700'>
           <p className='text-2xl text-gray-600 font-medium'>Ready to glow?</p>
